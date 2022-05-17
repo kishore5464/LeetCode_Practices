862f4ede0a89aa6ca71905aa4255e9e84e85b4bb
Kishore Kumar <kishorekumar@Kishores-MacBook-Air.local>
2022-05-17 23:49:32 +0530

    Defanged IP program added

--- src/com/kishore/string/Defanged_IPs.java
+++ src/com/kishore/string/Defanged_IPs.java
@@ -1,12 +1,14 @@
-package com.kishore.leetcode;
+package com.kishore.string;
 
 public class Defanged_IPs {
+	
     public static void main(String[] args) {
         defangedIps("120.90.80.114");
     }
-
+    
     public static void defangedIps(String ip) {
         String defangedIp = ip.replace(".","[.]");
         System.out.println(defangedIp);
     }
+    
 }
