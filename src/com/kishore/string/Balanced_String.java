0c77227389baf576ff95897277a69642ce2a1ec7
Kishore Kumar <kishorekumar@Kishores-MacBook-Air.local>
2022-05-19 01:22:34 +0530

    L & R String is balanced program added

---
+++ src/com/kishore/string/Balanced_String.java
@@ -0,0 +1,27 @@
+package com.kishore.string;
+
+public class Balanced_String {
+
+	public static void main(String[] args) {
+		balanceString("RLRRLLRLRLRRLLRL");
+	}
+
+	public static void balanceString(String s) {
+		int result = 0, count = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'L') {
+				count += 1;
+			} else {
+				count -= 1;
+			}
+
+			if (count == 0) {
+				result += 1;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
